4a7d23d5156d627242ba05868d59305273fa34d9
杨操 <yangcao@boco.com.cn>
2018-02-01 10:32:07 +0800

    杨操 <yangcao@boco.com.cn>

--- src/main/java/com/team/cd/common/utils/file/FileHelper.java
+++ src/main/java/com/team/cd/common/utils/file/FileHelper.java
@@ -4,131 +4,29 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.text.MessageFormat;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.net.URL;
+import java.nio.channels.FileChannel;
+
+import org.apache.commons.lang3.ClassUtils;
+
 public class FileHelper {
+	public static void main(String[] args) throws IOException {
+		System.out.println("文件复制开始");
+		long startTime = System.currentTimeMillis();
+		ClassLoader classLoader = ClassUtils.class.getClassLoader();
+		URL url = classLoader.getResource("./RandomAccessFile.txt");
+		File f = new File(url.getPath());
+		FileInputStream fileIns = new FileInputStream(f);
+		FileOutputStream fileOut = new FileOutputStream(new File("D:\\RandomAccessFile2.txt"));
+		FileChannel fromChannel = fileIns.getChannel();
+		FileChannel toChannel = fileOut.getChannel();
+		fromChannel.transferTo(0, fromChannel.size(), toChannel);
 
-	/**
-	 * Locale specific strings displayed to the user.
-	 *
-	 * @since ostermillerutils 1.00.00
-	 */
-	protected static ResourceBundle labels = ResourceBundle.getBundle("com.Ostermiller.util.FileHelper",  Locale.getDefault());
+		fromChannel.close();
+		fileIns.close();
+		toChannel.close();
+		fileOut.close();
 
-
-	/**
-	 * Move a file from one location to another.  An attempt is made to rename
-	 * the file and if that fails, the file is copied and the old file deleted.
-	 *
-	 * If the destination file already exists, an exception will be thrown.
-	 *
-	 * @param from file which should be moved.
-	 * @param to desired destination of the file.
-	 * @throws IOException if an error occurs.
-	 *
-	 * @since ostermillerutils 1.00.00
-	 */
-	public static void move(File from, File to) throws IOException {
-		move(from, to, false);
-	}
-
-	/**
-	 * Move a file from one location to another.  An attempt is made to rename
-	 * the file and if that fails, the file is copied and the old file deleted.
-	 *
-	 * @param from file which should be moved.
-	 * @param to desired destination of the file.
-	 * @param overwrite If false, an exception will be thrown rather than overwrite a file.
-	 * @throws IOException if an error occurs.
-	 *
-	 * @since ostermillerutils 1.00.00
-	 */
-	public static void move(File from, File to, boolean overwrite) throws IOException {
-		if (to.exists()){
-			if (overwrite){
-				if (!to.delete()){
-					throw new IOException(
-						MessageFormat.format(
-							labels.getString("deleteerror"),
-							(Object[])new String[] {
-								to.toString()
-							}
-						)
-					);
-				}
-			} else {
-				throw new IOException(
-					MessageFormat.format(
-						labels.getString("alreadyexistserror"),
-						(Object[])new String[] {
-							to.toString()
-						}
-					)
-				);
-			}
-		}
-
-		if (from.renameTo(to)) return;
-
-		InputStream in = null;
-		OutputStream out = null;
-		try {
-			in = new FileInputStream(from);
-			out = new FileOutputStream(to);
-			copy(in, out);
-			in.close();
-			in = null;
-			out.flush();
-			out.close();
-			out = null;
-			if (!from.delete()){
-				throw new IOException(
-					MessageFormat.format(
-						labels.getString("deleteoriginalerror"),
-						(Object[])new String[] {
-							from.toString(),
-							to.toString()
-						}
-					)
-				);
-			}
-		} finally {
-			if (in != null){
-				in.close();
-				in = null;
-			}
-			if (out != null){
-				out.flush();
-				out.close();
-				out = null;
-			}
-		}
-	}
-
-	/**
-	 * Buffer size when reading from input stream.
-	 *
-	 * @since ostermillerutils 1.00.00
-	 */
-	private final static int BUFFER_SIZE = 1024;
-
-	/**
-	 * Copy the data from the input stream to the output stream.
-	 *
-	 * @param in data source
-	 * @param out data destination
-	 * @throws IOException in an input or output error occurs
-	 *
-	 * @since ostermillerutils 1.00.00
-	 */
-	private static void copy(InputStream in, OutputStream out) throws IOException {
-		byte[] buffer = new byte[BUFFER_SIZE];
-		int read;
-		while((read = in.read(buffer)) != -1){
-			out.write(buffer, 0, read);
-		}
+		System.out.println("文件复制完毕，耗时（微妙）：" + (System.currentTimeMillis() - startTime));
 	}
 }
